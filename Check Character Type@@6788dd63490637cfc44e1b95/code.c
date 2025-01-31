5fb365a74e5b2c6d6396861d7cfaa322931113f2
ritika-jindal1127 <ritika0693.becse24@chitkara.edu.in>
2025-01-31 11:02:46 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -4,9 +4,15 @@ int main()
     
     char a;
     scanf("%c",&a);
-    if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
-    printf("Vowel");
-    else if(a!=a,e,i,o,u)
-    printf("Consonant");
+    if ((a >='a' && a<='z') || (a>='A' && a<='Z')){
+    (a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A'|| a=='E' || a=='I' || a=='O' || a=='U'){
+        printf("Vowel");
+    } else 
+        printf("Consonant");
+    }
+    else if ( a>='0' && a<='9')
+    printf("Digit");
+    else
+    printf("Character");
     return 0;
 }
