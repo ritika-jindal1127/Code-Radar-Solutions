fb8e93d0a5414af9eebeaf308a2d0f77d8619852
ritika-jindal1127 <ritika0693.becse24@chitkara.edu.in>
2025-03-09 13:31:48 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -18,3 +18,16 @@ void printArray(int arr[],int n){
         printf("%d ",arr[m]);
     }
 }
+/*
+First Pass (i = 0):
+
+Compare arr[0] (5) and arr[1] (3): Since 5 > 3, swap them. Array becomes: [3, 5, 8, 6, 2].
+
+Compare arr[1] (5) and arr[2] (8): Since 5 < 8, no swap. Array remains: [3, 5, 8, 6, 2].
+
+Compare arr[2] (8) and arr[3] (6): Since 8 > 6, swap them. Array becomes: [3, 5, 6, 8, 2].
+
+Compare arr[3] (8) and arr[4] (2): Since 8 > 2, swap them. Array becomes: [3, 5, 6, 2, 8].
+Result after Pass 1: [3, 5, 6, 2, 8]
+*/
+//loop will exceute i=0,1,2,3,4 becoz n is 5 jab tak ascending order nhi ho jata loop will work
